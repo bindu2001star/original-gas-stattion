47f82987923ef0b69846492780f7c34763b23478
bindu2001star <himabindusambangi@gmail.com>
2023-03-14 10:33:04 +0530

    userandlocal

---
+++ main.js
@@ -0,0 +1,5 @@
+//var box=document.getElementById("my-form");
+//box.style.backgroundColor="#f4f4f4";
+//var adding=document.getElementById("users");
+//var multy=document.getElementById("my-form").value;
+//adding.appendChild(multy);
