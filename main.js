f35750924a3b6f563f3383238557754299613b29
bindu2001star <70961642+bindu2001star@users.noreply.github.com>
2023-03-21 11:27:41 +0530

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-//var box=document.getElementById("my-form");
-//box.style.backgroundColor="#f4f4f4";
-//var adding=document.getElementById("users");
-//var multy=document.getElementById("my-form").value;
-//adding.appendChild(multy);
